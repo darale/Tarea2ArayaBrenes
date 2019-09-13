2aa023129a6bdecac45fe103dffc62951dd68b5c
darale <43095656+darale@users.noreply.github.com>
2019-09-13 17:35:08 -0600

    Add files via upload

---
+++ Flake8Errors.py
@@ -0,0 +1,14 @@
+#Codigo con errores detectables por el flake8
+
+import math
+
+def ope(a,b):
+    c=a+b
+    return c
+
+x=int
+d=int(input("Ingrese un número: "))
+e=int(input("Ingrese otro número: "))
+print(ope(d,e))
+
+k=input("Presione una tecla para salir")
